f2ce27cc348123bd4c2a4aadabec7c4cc0c0fd41
Firstname Lastname <your_email@example.com>
2019-12-02 17:15:52 +0800

    init my project

---
+++ src/views/form/MyBasicForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <a-card :body-style="{padding: '24px 32px'}" :bordered="false">
+    <a-form @submit="handleSubmit" :form="form">
+        <a-button htmlType="submit" type="primary">添加字段</a-button>
+        <a-button style="margin-left: 8px">保存</a-button>
+    </a-form>
+  </a-card>
+</template>
+
+<script>
+export default {
+  name: 'BaseForm',
+  data () {
+    return {
+      description: '表单页用于向用户收集或验证信息，基础表单常见于数据项较少的表单场景。',
+      value: 1,
+
+      // form
+      form: this.$form.createForm(this)
+
+    }
+  },
+  methods: {
+
+    // handler
+    handleSubmit (e) {
+      e.preventDefault()
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          // eslint-disable-next-line no-console
+          console.log('Received values of form: ', values)
+        }
+      })
+    }
+  }
+}
+</script>
---
+++ src/views/newPage/newPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-button>新增</a-button>
+</template>
+
+<script>
+export default {
+  name: 'newPage'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
